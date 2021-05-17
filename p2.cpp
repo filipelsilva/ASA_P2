c77c7af3f177f1a6a59daa169a5e8ea82b350d95
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-05-17 11:35:25 +0100

    cleaned stuff

--- p2.cpp
+++ p2.cpp
@@ -4,9 +4,6 @@
 
 using namespace std;
 
-#define SOURCE -1
-#define SINK -2
-
 enum colors { WHITE, GREY, BLACK };
 
 class Process {
@@ -23,26 +20,20 @@ class Process {
 class Program {
 	public:
 		int numProcesses = -1;
+		int source = -1;
+		int sink = -1;
 		vector<Process*> processes;
 
-		/* Costs of communication between/operation of processes */
-		vector<int> costsX;
-		vector<int> costsY;
 		vector<vector<int>> commsCost;
-
-		vector<int> fluxX;
-		vector<int> fluxY;
 		vector<vector<int>> flux;
 
 		Program(int nP) {
-			this->numProcesses = nP + 2;
+			this->numProcesses = nP;
+			this->source = nP;
+			this->sink = nP + 1;
 			this->processes = vector<Process*>(nP + 2);
-			this->costsX = vector<int>(nP, 0);
-			this->costsY = vector<int>(nP, 0);
-			this->commsCost = vector<vector<int>>(nP, vector<int>(nP, 0));
-			this->fluxX = vector<int>(nP, 0);
-			this->fluxY = vector<int>(nP, 0);
-			this->flux = vector<vector<int>>(nP, vector<int>(nP, 0));
+			this->commsCost = vector<vector<int>>(nP + 2, vector<int>(nP, 0));
+			this->flux = vector<vector<int>>(nP + 2, vector<int>(nP, 0));
 		}
 
 		int capacity(int id1, int id2);
@@ -51,16 +42,7 @@ class Program {
 };
 
 int Program::capacity(int id1, int id2) {
-	if (id1 == SOURCE) /* -1 is the X node */
-		return costsX[id2];
-	else if (id2 == SOURCE)
-		return costsX[id1];
-	else if (id1 == SINK) /* SINK is the Y node */
-		return costsY[id2];
-	else if (id2 == SINK)
-		return costsY[id1];
-	else /* Otherwise, values between two processes */
-		return commsCost[id1][id2];
+	return commsCost[id1][id2];
 }
 
 void Program::DFS_Visit(Process* n) {
@@ -97,13 +79,9 @@ Program parseData() {
 	for (z = 0; z < n; z++) {
 		ret.processes[z] = new Process(z);
 		scanf("%d %d", &px, &py);
-		ret.costsX[z] = px;
-		ret.costsY[z] = py;
+		ret.commsCost[ret.source][z] = px;
+		ret.commsCost[ret.sink][z] = py;
 	}
-
-	/* Adding Processor X and Y */
-	ret.processes[z] = new Process(SOURCE);
-	ret.processes[z+1] = new Process(SINK);
 
 	int i, j, c;
 	for (z = 0; z < k; z++) {
@@ -122,7 +100,12 @@ Program parseData() {
 
 int main(int argc, char *argv[]) {
 	Program program = parseData();
-	printf("%d\n", program.processes[program.numProcesses-1]->id);
+	for (vector<int> vec : program.commsCost) {
+		for (int i : vec) {
+			printf("%d\t", i);
+		}
+		printf("\n");
+	}
 	program.clean();
 	return 0;
 }
