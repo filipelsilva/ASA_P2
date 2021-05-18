12b9863824da30739995e6b06c0327a39d6c894c
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-05-18 11:29:19 +0100

    making shit work

--- p2.cpp
+++ p2.cpp
@@ -10,9 +10,11 @@ class Process {
 	public:
 		int id;
 		colors color = WHITE;
+		Process* father;
 		vector<Process*> neighbours = vector<Process*>();
 
 		Process(int id) {
+			this->father = this;
 			this->id = id;
 		}
 };
@@ -23,9 +25,7 @@ class Program {
 		int source = -1;
 		int sink = -1;
 		vector<Process*> processes;
-
 		vector<vector<int>> weights;
-		vector<vector<int>> flux;
 		vector<vector<int>> residual;
 
 		Program(int nP) {
@@ -39,43 +39,57 @@ class Program {
 
 		int getWeights(int id1, int id2);
 		int getResidual(int id1, int id2);
-		void DFS();
-		int maxFlux(vector<Process*> path);
-		int fordFulkerson();
+		vector<Process*> DFS();
+		//int maxFlux(vector<Process*> path);
+		//int fordFulkerson();
 		void clean();
 };
 
 int Program::getWeights(int id1, int id2) {
+	if (id2 == this->source || id2 == this->sink)
+		return weights[id2][id1];
+
 	return weights[id1][id2];
 }
 
 int Program::getResidual(int id1, int id2) {
+	if (id2 == this->source || id2 == this->sink)
+		return weights[id2][id1];
+
 	return residual[id1][id2];
 }
 
-void Program::DFS() {
+vector<Process*> Program::DFS() {
 	stack<Process*> toVisit;
+	vector<Process*> path;
 	toVisit.push(this->processes[this->source]);
 
-	for (Process* p : this->processes)
-		p->color = WHITE;
+	//for (Process* p : this->processes)
+	//	p->color = WHITE;
 
 	while (!toVisit.empty()) {
 		Process* node = toVisit.top();
-		node->color = GREY;
+		path.push_back(node);
 
+		if (node == this->processes[this->sink]) {
+			/*
+			for (Process* p : path)
+				printf("%d\t", p->id);
+			printf("\n");
+			*/
+			return path;
+		}
 		//printf("Vizinhos de %d\n", node->id);
 		for (Process* next : node->neighbours) {
 			//printf("\t%d\n", next->id);
-			if (next->color == WHITE) {
+			if (getResidual(node->id, next->id) && next->father != node) {
 				toVisit.push(next);
-				next->color = RED;
+				next->father = node;
 			}
 		}
 
 		if (toVisit.top() == node) {
 			node->color = BLACK;
-			printf("%d\n", node->id);
 			toVisit.pop();
 		}
 	}
@@ -95,13 +109,13 @@ int Program::maxFlux(vector<Process*> path) {
 
 	return flux;
 }
-
+*/
 int Program::fordFulkerson() {
 	vector<Process*> augmentation;
 	int flux = 0;
 
 	while (true) {
-		augmentation = 0;//DFS(this->processes[this->source]);
+		augmentation = DFS();
 
 		if (augmentation.empty())
 			return flux;
@@ -111,7 +125,6 @@ int Program::fordFulkerson() {
 		}
 	}
 }
-*/
 
 void Program::clean() {
 	for (Process* n : this->processes)
@@ -134,6 +147,8 @@ Program parseData() {
 		scanf("%d %d", &px, &py);
 		ret.weights[ret.source][z] = px;
 		ret.weights[ret.sink][z] = py;
+		ret.residual[ret.source][z] = px;
+		ret.residual[ret.sink][z] = py;
 		ret.processes[ret.source]->neighbours.push_back(ret.processes[z]);
 		ret.processes[z]->neighbours.push_back(ret.processes[ret.sink]);
 	}
@@ -143,11 +158,8 @@ Program parseData() {
 		scanf("%d %d %d", &i, &j, &c);
 		ret.weights[i-1][j-1] = c;
 		ret.weights[j-1][i-1] = c;
-
-		if (c != 0) {
-			ret.processes[i-1]->neighbours.push_back(ret.processes[j-1]);
-			ret.processes[j-1]->neighbours.push_back(ret.processes[i-1]);
-		}
+		ret.processes[i-1]->neighbours.push_back(ret.processes[j-1]);
+		ret.processes[j-1]->neighbours.push_back(ret.processes[i-1]);
 	}
 
 	return ret;
@@ -155,9 +167,13 @@ Program parseData() {
 
 int main(int argc, char *argv[]) {
 	Program program = parseData();
-	/*for (Process* p : program.processes)
-		printf("%d\n", p->id);
-	for (vector<int> vec : program.weights) {
+	/*for (Process* p : program.processes) {
+		printf("Vizinhos de %d\n", p->id + 1);
+		for (Process* pp : p->neighbours)
+			printf("%d\t", pp->id + 1);
+		printf("\n");
+	}*/
+	/*for (vector<int> vec : program.weights) {
 		for (int i : vec)
 			printf("%d\t", i);
 		printf("\n");
